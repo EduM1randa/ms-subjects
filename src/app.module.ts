7cf4f64ef2e91e4efc3c736fd09158bd6c705bde
Edu Miranda C <eduardo.miranda01@alumnos.ucn.cl>
2024-09-13 00:24:16 -0300

    mongo key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SubjectsModule } from './subjects/subjects.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     SubjectsModule, 
-    MongooseModule.forRoot("mongodb+srv://codenative:c0d3n4t1v3@cluster0.zdt5d.mongodb.net/ms-subjects?retryWrites=true&w=majority&appName=Cluster0")
+    MongooseModule.forRoot(process.env.MONGO_KEY)
   ],
   controllers: [],
   providers: [],
